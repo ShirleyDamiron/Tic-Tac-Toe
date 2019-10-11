1b97bd59c2c151ad210b8fe5695a19306806f930
Shirley Damiron <shirleydamiron@Administrators-MacBook-Pro.local>
2019-10-10 20:08:45 -0400

    Updates

---
+++ precache-manifest.be94e930591055c4aa61d7c2fa917632.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5b76e2f4b9c4f2668ba92e60686ee49",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "21a728ff41828ac5439e",
+    "url": "/Tic-Tac-Toe/static/css/main.9151d181.chunk.css"
+  },
+  {
+    "revision": "5a704ff89b7b786c217f",
+    "url": "/Tic-Tac-Toe/static/js/2.6aab3cfe.chunk.js"
+  },
+  {
+    "revision": "21a728ff41828ac5439e",
+    "url": "/Tic-Tac-Toe/static/js/main.31939b95.chunk.js"
+  },
+  {
+    "revision": "ed086bc0c20210bbb52a",
+    "url": "/Tic-Tac-Toe/static/js/runtime~main.28f68352.js"
+  }
+]);
