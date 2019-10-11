741ef9aa3224327cce5be9df41e60cff3aa1482a
Shirley Damiron <shirleydamiron@Administrators-MacBook-Pro.local>
2019-10-10 20:15:44 -0400

    adding project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Game from './Components/Game/Game';
+
+function App() {
+  return (
+    <div>
+      <Game />
+    </div>
+   
+  );
+}
+
+export default App
